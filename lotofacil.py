2b9fc6103452e9a9cb862e3d74472519452ef00a
Luiz dos Santos <67069686+luiz1990santos@users.noreply.github.com>
2022-06-21 11:44:02 -0300

    Update lotofacil.py

--- lotofacil.py
+++ lotofacil.py
@@ -8,8 +8,7 @@ try:
     print('\nDigite seus 15 números')
     numero=[]
     for contador in range(1,16):
-        numero.append(int(input(f'Digite o {contador}º:')))#append para mostrar
-        #todos os números no print
+        numero.append(int(input(f'Digite o {contador}º:')))#append para mostrar todos os números no print.
         numeroSorteados = random.sample(range(1,25),15)
     print(f'\nSeus números são {numero}')
     print(f'Números sorteados: {sorted(numeroSorteados, key=int)}')
